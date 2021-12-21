4ed6644fc3d406f9146ec3cb12a18ad0c0916c0f
Альбина <91634815+AlbinaKarimova@users.noreply.github.com>
2021-12-21 19:16:04 +0500

    Add files via upload

---
+++ dz_6.3.cpp
@@ -0,0 +1,53 @@
+
+//Составьте программу, меняющую местами элементы матрицы симметрично побочной диагонали
+#include <iostream>
+
+using namespace std;
+
+void print_array(int** array, int s) {
+    for (int i = 0;i < s;i++) {
+        for (int j = 0;j < s;j++) {
+            cout << array[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void random_array(int** array, int s) {
+    for (int i = 0;i < s;i++) {
+        for (int j = 0;j < s;j++) {
+            array[i][j] = rand() % 10+1;
+        }
+    }
+}
+
+void change_array(int** array, int s) {
+    for (int i = 0;i < s;i++) {
+        for (int j = 0;j < s - i - 1;j++) {
+            int temp;
+            temp = array[i][j];
+            array[i][j] = array[s - j - 1][s - i - 1];
+            array[s - j - 1][s - i - 1] = temp;
+        }
+    }
+
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int s;
+    cin >> s;
+    int** array = new int* [s];
+    for (int i = 0;i < s;i++) {
+        array[i] = new int[s];
+    }
+
+    random_array(array, s);
+    cout << "Исходная матрица" << endl;
+    print_array(array, s);
+    change_array(array, s);
+    cout << "Матрица симметричная относительно побочной диагонали" << endl;
+    print_array(array, s);
+    return 0;
+}
