7c19c4e7378724075679222538fb30c8ed1983c0
Альбина <91634815+AlbinaKarimova@users.noreply.github.com>
2021-12-22 15:41:28 +0500

    Add files via upload

---
+++ dz_6.4.cpp
@@ -0,0 +1,67 @@
+/*В квадратной таблице обменяйте местами элементы строки и столбца,
+на пересечении которых находится минимальный из положительных элементов*/
+#include<iostream>
+using namespace std;
+void print_array(int** array, int s) {
+	for (int i = 0;i < s;i++) {
+		for (int j = 0;j < s;j++) {
+			cout << array[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void random_array(int** array, int s) {
+	for (int i = 0;i < s;i++) {
+		for (int j = 0;j < s;j++) {
+			array[i][j] = rand() % 10 + 1;
+		}
+	}
+}
+void change_array(int** array, int s) {
+	int i_min = 0, j_min = 0;
+	for (int i = 0;i < s; i++) {
+		for (int j = 0; j < s; j++) {
+			if (array[i][j] < array[i_min][j_min]) {
+				i_min = i;
+				j_min = j;
+			}
+		}
+	}
+	for (int i = 0;i < s;i++) {
+		for (int j = 0;j < s;j++) {
+			if (i != i_min && j != j_min) {
+				int temp;
+				temp = array[i][j_min];
+				array[i][j_min] = array[i_min][j];
+				array[i_min][j] = temp;
+			}
+		}
+
+	}
+	cout << "Матрица после преобразований" << endl;
+	for (int i = 0;i < s;i++) {
+		for (int j = 0;j < s;j++) {
+			cout << array[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	int s;
+	cin >> s;
+	int** array = new int* [s];
+	for (int i = 0;i < s;i++) {
+		array[i] = new int[s];
+	}
+	random_array(array, s);
+	cout << "Исходная матрица" << endl;
+	print_array(array, s);
+	change_array(array, s);
+	return 0;
+}
