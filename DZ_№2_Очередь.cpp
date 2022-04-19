e740476499f67716e88c97029b428fca3c5270ba
Альбина <91634815+AlbinaKarimova@users.noreply.github.com>
2022-04-20 02:05:09 +0500

    Update DZ_№2_Очередь.cpp

--- DZ_№2_Очередь.cpp
+++ DZ_№2_Очередь.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 struct man {
-	char name[50];
+	string name;
 	int age;
-	char work[50];
-	int salary;
-	char goal[50];
+	string gender;
+	string work;
+	string salary;
+	string goal;
 };
 
 struct elem {
-	man m;
+	man value;
 	elem* next = nullptr;
 };
 
@@ -22,34 +23,22 @@ struct queue {
 	elem* tail = nullptr;
 	size_t length = 0;
 };
-
-inline man create_elem(const char* name,int age,const char* work, int salary, const char* goal) {
-	man m;
-	strcpy_s(m.name, name);
-	m.age = age;
-	strcpy_s(m.work, work);
-	m.salary = salary;
-	strcpy_s(m.goal, goal);
-	return m;
-}
-
-void enqueue(queue& queue, const char* name,int age, const char* work, int salary, const char* goal) {
-	man newm = create_elem(name,age,work,salary,goal);
+void enqueue(queue& q, man m) {
 	elem* newel = new elem;
-	newel->m = newm;
-	if (!queue.tail) {
-		queue.head = queue.tail = newel;
+	newel->value = m;
+	if (!q.tail) {
+		q.head = q.tail = newel;
 	}
 	else {
-		queue.tail->next = newel;
-		queue.tail = newel;
+		q.tail->next = newel;
+		q.tail = newel;
 	}
-	queue.length++;
+	q.length++;
 }
 
 bool dequeue(queue& queue, man& m) {
 	if (!queue.head) return false;
-	m = queue.head->m;
+	m = queue.head->value;
 	elem* rem = queue.head;
 	queue.head = queue.head->next;
 	delete rem;
@@ -68,51 +57,39 @@ void clear(queue& queue) {
 	queue.tail = nullptr;
 }
 
-void load_data(const char* filename, queue& kredit, queue& vklad) {
-	ifstream f(filename);
-	if (f.is_open()) {
-		while (!f.eof()) {
-			char* man = new char[300];
-			f.getline(man, 300);
-			char* name = new char[50];
-			char* goal = new char[50];
-			int age;
-			char* work = new char[50];
-			int salary;
-			sscanf_s(man, "%49[^0-9] %d %49[^0-9] %d %49[^0-9]", name, 50,&age,work,50,&salary, goal, 50);
-			int l = strlen(goal);
-			if (goal[l - 1] == 'д') {
-				enqueue(vklad, name,age,work,salary,goal);
-			}
-			else if (goal[l - 1] == 'т') {
-				enqueue(kredit, name,age,work,salary,goal);
-			}
-		}
-		f.close();
-	}
-}
-
-void show_data(queue& q) {
-	int i = 0;
-	while (q.length > 0) {
-		man m;
-		if (dequeue(q, m)) {
-			cout << ++i << ")" << m.name <<" Возраст:" << m.age <<" Место работы:"<<m.work<< " Доход:"<<m.salary << " Цель : " << m.goal;
-			cout << endl;
-		}
-	}
-}
-
-void main() {
+int main() {
 	setlocale(LC_ALL, "rus");
 	queue kredit;
 	queue vklad;
-	load_data("data.txt", kredit, vklad);
-	cout << "Люди, взявшие кредит:";
+	ifstream f("data.txt");
+	ofstream fi("vklad.txt");
+	ofstream fio("kredit.txt");
+	if (f.is_open() && fi.is_open() && fio.is_open()) {
+		while (!f.eof()) {
+			man m;
+			f >> m.name >> m.gender >> m.age >> m.work >> m.salary >> m.goal;
+			if (m.goal == "Вклад") {
+				enqueue(vklad, m);
+				fi << m.name << " " << m.gender << " " << m.age << " " << m.work << " " << m.salary << " " << m.goal << endl;
+
+			}
+			else if (m.goal == "Кредит") {
+				enqueue(kredit, m);
+				fio << m.name << " " << m.gender << " " << m.age << " " << m.work << " " << m.salary << " " << m.goal << endl;
+			}
+		}
+
+	}
+	else cout << "ERROR";
+	f.close();
+	fi.close();
+	fio.close();
+	cout << "Папка с людьми, взявшими кредит:";
 	cout << endl;
-	show_data(kredit);
+	cout << "kredit.txt";
 	cout << endl;
-	cout << "Лица, взявшие вклад:";
+	cout << "Папка с людьми, взявшими вклад:";
 	cout << endl;
-	show_data(vklad);
+	cout << "vklad.txt";
+	return 0;
 }
